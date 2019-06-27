72b7c3feffd21d9c38efeb127c978f52e222a094
Katie Vogel <kvog983@gmail.com>
2019-06-27 09:00:19 -0400

    fixed repeating word

--- word-game2.js
+++ word-game2.js
@@ -1,32 +1,33 @@
 var wins = 0;
 
-function setup(word) {
-    var listElement = document.querySelector('.word')
-    for (var i = 0; i < word.length; i++) {
-        listElement.append(word[i]);
+var chancesLeft;
+
+var underscores = [];
+var letterCount = function (word) {
+    underscores = [];
+    for (i = 0; i < word.length; i++) {
+        underscores[i] = "_";
+        chancesLeft = word.length + 3;
+        document.querySelector('.num-chances').innerHTML = chancesLeft;
     }
+    document.querySelector('.word').innerHTML = underscores.join(" ");
 }
+
+// function setup(word) {
+//     var listElement = document.querySelector('.word')
+//     for (var i = 0; i < word.length; i++) {
+//         listElement.append(word[i]);
+//     }
+// }
 var wordList = ['GRIZZLYBEAR', 'MONGOOSE', 'CHEETAH', 'ARMADILLO', 'ORCA', 'ORYX', 'KANGAROO', 'PANGOLIN', 'MANATEE', 'QUOKKA', 'WALLABE', 'OSTRICH', 'BISON', 'LION', 'COBRA', 'RATTLESNAKE', 'ANENOME', 'PORCUPINE', 'HEDGEHOG', 'WOLF', 'TIGER', 'HYENA', 'DOLPHIN', 'CRAB', 'LOBSTER', 'KOALA', 'JELLYFISH', 'ALBATROSS', 'CONDOR', 'RAVEN', 'NARWHAL', 'LEOPARD', 'JAGUAR', 'CAPYBARA', 'CROCODILE', 'ALLIGATOR', 'GAZELLE', 'CAMEL', 'HORSE', 'DONKEY'];
 var word = randomWord(wordList);
-setup(word);
+letterCount(word);
 
 function randomWord(wordList) {
     return wordList[Math.floor(Math.random() * wordList.length)];
 }
 
-var underscores = [];
-var letterCount = function (word) {
-    for (i = 0; i < word.length; i++) {
-        underscores[i] = "_";
-    }
-    document.querySelector('.word').innerHTML = underscores.join(" ");
-}
-
-
-letterCount(word);
 var wrongGuess = [];
-var chancesLeft = word.length + 3;
-document.querySelector('.num-chances').innerHTML = chancesLeft;
 document.onkeyup = function (guess) {
     if (chancesLeft > 0) {
         if (!guess.key.toUpperCase().match(/^[A-Za-z]$/)) {
@@ -54,9 +55,8 @@ document.onkeyup = function (guess) {
 
 document.querySelector('.btn').onclick =
     function nextGame() {
-        var newWord = randomWord(wordList);
-        var foo = setup(newWord);
-        word.replace(foo, newWord);
+        word = randomWord(wordList);
+        var newUnderscores = letterCount(word);
     }
 
 
